40c323748fda2241d8f7c6380c64b4f480289f5d
donsonioc2010 <whddnjs822@gmail.com>
2022-10-22 17:38:43 +0900

    feat : 소환사명 공백 치환 기능 구현

---
+++ src/main/kotlin/com/example/yourggtask/global/utils/StringUtils.kt
@@ -0,0 +1,21 @@
+package com.example.yourggtask.global.utils
+
+import org.apache.commons.lang3.StringUtils
+import org.springframework.util.ObjectUtils
+
+
+class StringUtils {
+    companion object {
+        //Optional로 간단하게 해결이 가능할 것 같은데 생각이 안난다..
+        fun summonerNameWhiteSpaceReplace(summonerName: String): String {
+            if (ObjectUtils.isEmpty(summonerName))
+                throw IllegalArgumentException("[StringUtils] : SummonerName Is Null")
+
+            var trimSummonerName: String = summonerName.trim()
+            if (StringUtils.isBlank(trimSummonerName))
+                throw IllegalArgumentException("[StringUtils] : SummonerName Is Blank")
+
+            return trimSummonerName.replace(" ", "")
+        }
+    }
+}
