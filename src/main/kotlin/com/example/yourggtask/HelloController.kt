9928654f5c6e26c83e6fc7b965bad10a622ff694
donsonioc2010 <whddnjs822@gmail.com>
2022-10-22 16:34:37 +0900

    feat : RiotApi Bean Set

--- src/main/kotlin/com/example/yourggtask/HelloController.kt
+++ src/main/kotlin/com/example/yourggtask/HelloController.kt
@@ -1,13 +1,26 @@
 package com.example.yourggtask
 
+import com.example.yourggtask.global.utils.RiotConstants
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Test
+ */
+@Slf4j
 @RestController
-class HelloController {
+class HelloController(
+    private val riotConstants: RiotConstants
+) {
+    val logger: Logger = LoggerFactory.getLogger(HelloController::class.java)
 
     @GetMapping("/")
     fun hello(): String {
+        logger.info(riotConstants.toString())
+
         return "hello"
     }
 }
