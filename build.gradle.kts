91309db94100c9151368a21e8da7aa3ae4d23b69
donsonioc2010 <whddnjs822@gmail.com>
2022-10-22 16:55:56 +0900

    chore : Apache HttpClient사용위한 Dependencies추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
