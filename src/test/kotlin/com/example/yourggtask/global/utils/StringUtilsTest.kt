d53c86e639c4d7238b7d3ff071bfb8299b7dbac4
donsonioc2010 <whddnjs822@gmail.com>
2022-10-22 17:40:31 +0900

    test : 소환사명 변경기능 테스트케이스

---
+++ src/test/kotlin/com/example/yourggtask/global/utils/StringUtilsTest.kt
@@ -0,0 +1,32 @@
+package com.example.yourggtask.global.utils
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+internal class StringUtilsTest {
+    val logger: Logger = LoggerFactory.getLogger(StringUtilsTest::class.java)
+
+    @Test
+    fun `소환사명 공백이 존재하는 경우, 공백을 제거한 소환사명을 획득한다`() {
+        var orgSummonerName: String = "눕는게 일상 일상"
+
+        var newSummonerName = StringUtils.summonerNameWhiteSpaceReplace(orgSummonerName)
+
+        logger.info("[StringUtilsTest : 공백 테스트 , 정상] : OrgSummoner : $orgSummonerName, NewSummoner: $newSummonerName")
+
+        assertEquals(newSummonerName, "눕는게일상일상")
+    }
+
+    @Test
+    fun `소환사명이 공백만 있는 경우, Exception이 발생한다`() {
+        var summonerName: String = ""
+        logger.info("[StringUtilsTest : 공백 Exception테스트] Test Summoner: ${summonerName}")
+        assertThrows(IllegalArgumentException::class.java) {
+            StringUtils.summonerNameWhiteSpaceReplace(summonerName)
+        }
+    }
+
+}
